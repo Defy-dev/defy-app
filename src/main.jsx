d88709b90cbb705a39f7bc3352aac7c757dafb33
Defy-dev <a.s.sherbinina@gmail.com>
2026-05-30 17:25:49 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Defy from '../defy.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Defy />
+  </React.StrictMode>
+)
